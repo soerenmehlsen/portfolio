db2dd4c24ec4cc3f9ec75a5131654fe7bb9f144a
Søren Mehlsen <78858933+soerenmehlsen@users.noreply.github.com>
2024-09-14 00:44:26 +0200

    Added hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Hero from "@/app/components/Hero";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center pt-20 md:pt-28 px-4">
-      Project
+      <Hero/>
     </main>
   );
 }
