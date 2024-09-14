22c4af4c0aa4ae792e04137601863ae875b14eab
Søren Mehlsen <78858933+soerenmehlsen@users.noreply.github.com>
2024-09-14 22:42:30 +0200

    Fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
-    basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
-    output: 'export'
+    output: 'export',
+    assetPrefix: process.env.ASSET_PREFIX,
+    basePath: process.env.BASE_PATH,
 };
 
 export default nextConfig;
