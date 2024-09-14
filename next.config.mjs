398f85e5de52ba4d175ac921f568b685aef4a803
Søren Mehlsen <78858933+soerenmehlsen@users.noreply.github.com>
2024-09-14 22:07:35 +0200

    add basePath & assetsPrefix to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: '/portfolio'
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
+    basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
 };
 
 export default nextConfig;
