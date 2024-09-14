f435a5939ffd6cf052f416375ebffd967d975874
Søren Mehlsen <78858933+soerenmehlsen@users.noreply.github.com>
2024-09-14 21:51:42 +0200

    Fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
-    distDir: "dist",
-    images: {
-        unoptimized: true,
-    },
 };
 
 export default nextConfig;
