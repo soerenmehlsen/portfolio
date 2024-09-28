ab72b344d9e96f6e81e726b411f66545533cdbc4
Søren Mehlsen <78858933+soerenmehlsen@users.noreply.github.com>
2024-09-28 15:48:34 +0200

    Add video-gif for the project card

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -1,121 +1,139 @@
-import {Link, ProjectInfo} from "@/app/lib/types";
+import { Link, ProjectInfo } from "@/app/lib/types";
 import { CgWorkAlt } from "react-icons/cg";
 import { LuGraduationCap } from "react-icons/lu";
 import React from "react";
-import { FaReact, FaCss3Alt, FaHtml5, FaJs, FaGitAlt, FaDocker, FaNodeJs, FaGithub } from "react-icons/fa";
-import { SiTypescript, SiMongodb, SiPostman, SiAzuredevops, SiKotlin, SiDotnet, SiMicrosoftsqlserver } from "react-icons/si";
+import {
+  FaReact,
+  FaCss3Alt,
+  FaHtml5,
+  FaJs,
+  FaGitAlt,
+  FaDocker,
+  FaNodeJs,
+  FaGithub,
+} from "react-icons/fa";
+import {
+  SiTypescript,
+  SiMongodb,
+  SiPostman,
+  SiAzuredevops,
+  SiKotlin,
+  SiDotnet,
+  SiMicrosoftsqlserver,
+} from "react-icons/si";
 import { GoCopilot } from "react-icons/go";
 import { AiOutlineOpenAI } from "react-icons/ai";
 
-
 export const links: Link[] = [
-    {
-        nameEng: "Home",
-        hash: "#home",
-    },
-    {
-        nameEng: "About",
-        hash: "#about",
-    },
-    {
-        nameEng: "Projects",
-        hash: "#projects",
-    },
-    {
-        nameEng: "Skills",
-        hash: "#skills",
-    },
-    {
-        nameEng: "Experience",
-        hash: "#experience",
-    },
+  {
+    nameEng: "Home",
+    hash: "#home",
+  },
+  {
+    nameEng: "About",
+    hash: "#about",
+  },
+  {
+    nameEng: "Projects",
+    hash: "#projects",
+  },
+  {
+    nameEng: "Skills",
+    hash: "#skills",
+  },
+  {
+    nameEng: "Experience",
+    hash: "#experience",
+  },
 ];
 
 export const projectsData: ProjectInfo[] = [
-    {
-        title: "DeepDR",
-        description: "DeepDR is a program trained with deep learning model to detect diabetic retinopathy in retinal fundus images.",
-        techStack: ["Matlab"],
-        imageUrl: "/DeepDR.png",
-        github: "https://github.com/soerenmehlsen/DeepDR",
-        live: "",
-    },
-    {
-        title: "Upcoming Project",
-        description: "Stay tuned for my upcoming project!",
-        techStack: ["React", "TypeScript", "TailwindCSS"],
-        imageUrl: "/upcoming-project.png",
-        github: "https://github.com",
-        live: "",
-    },
+  {
+    title: "DeepDR",
+    description:
+      "DeepDR is a program trained with deep learning model to detect diabetic retinopathy in retinal fundus images.",
+    techStack: ["Matlab"],
+    imageUrl: "/DeepDR.png",
+    imageGifUrl: "/DeepDR.gif",
+    github: "https://github.com/soerenmehlsen/DeepDR",
+    live: "https://github.com/soerenmehlsen/DeepDR",
+  },
+  {
+    title: "Upcoming Project",
+    description: "Stay tuned for my upcoming project!",
+    techStack: ["React", "TypeScript", "TailwindCSS"],
+    imageUrl: "/upcoming-project.png",
+    github: "https://github.com",
+    live: "https://github.com/soerenmehlsen/DeepDR",
+  },
 ];
 
 export const skillsData = [
-    {
-        title: "Frontend",
-        skills: [
-            { name: "React.js", icon: FaReact },
-            { name: "CSS", icon: FaCss3Alt },
-            { name: "HTML", icon: FaHtml5 },
-            { name: "TypeScript", icon: SiTypescript },
-            { name: "JavaScript", icon: FaJs },
-            { name: "WPF", icon: SiDotnet },
-            { name: "Kotlin", icon: SiKotlin }
-        ],
-    },
-    {
-        title: "Backend",
-        skills: [
-            { name: "C#", icon: SiDotnet },
-            { name: ".NET Core", icon: SiDotnet },
-            { name: "MSSQL", icon: SiMicrosoftsqlserver },
-            { name: "MongoDB", icon: SiMongodb },
-            { name: "REST API", icon: FaNodeJs },
-            { name: "EF Core", icon: SiDotnet }
-        ],
-    },
-    {
-        title: "DevOps",
-        skills: [
-            { name: "Git", icon: FaGitAlt },
-            { name: "GitHub", icon: FaGithub },
-            { name: "Postman", icon: SiPostman },
-            { name: "Docker", icon: FaDocker },
-            { name: "Azure", icon: SiAzuredevops }
-        ],
-    },
-    {
-        title: "AI",
-        skills: [
-            { name: "Copilot", icon: GoCopilot },
-            { name: "ChatGPT", icon: AiOutlineOpenAI }
-        ],
-    },
+  {
+    title: "Frontend",
+    skills: [
+      { name: "React.js", icon: FaReact },
+      { name: "CSS", icon: FaCss3Alt },
+      { name: "HTML", icon: FaHtml5 },
+      { name: "TypeScript", icon: SiTypescript },
+      { name: "JavaScript", icon: FaJs },
+      { name: "WPF", icon: SiDotnet },
+      { name: "Kotlin", icon: SiKotlin },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      { name: "C#", icon: SiDotnet },
+      { name: ".NET Core", icon: SiDotnet },
+      { name: "MSSQL", icon: SiMicrosoftsqlserver },
+      { name: "MongoDB", icon: SiMongodb },
+      { name: "REST API", icon: FaNodeJs },
+      { name: "EF Core", icon: SiDotnet },
+    ],
+  },
+  {
+    title: "DevOps",
+    skills: [
+      { name: "Git", icon: FaGitAlt },
+      { name: "GitHub", icon: FaGithub },
+      { name: "Postman", icon: SiPostman },
+      { name: "Docker", icon: FaDocker },
+      { name: "Azure", icon: SiAzuredevops },
+    ],
+  },
+  {
+    title: "AI",
+    skills: [
+      { name: "Copilot", icon: GoCopilot },
+      { name: "ChatGPT", icon: AiOutlineOpenAI },
+    ],
+  },
 ];
 
 export const experiencesData = [
-    {
-        title: "Graduated Health Technology Engineer",
-        location: "Aarhus University, DK",
-        description:
-            "Throuhout my studies, I took various software courses to learn about software development",
-        icon: React.createElement(LuGraduationCap),
-        date: "2020-2024",
-    },
-    {
-        title: "Engineering intern",
-        location: "Teknologi i Praksis, DK",
-        description:
-            "I was responsible for designing and 3D printing assistive devices. Gained experience in user involvement, technical presentations, and innovative problem-solving, with several solutions showcased in exhibitions.",
-        icon: React.createElement(CgWorkAlt),
-        date: "2023 - 2023",
-    },
-    {
-        title: "Sales assistant",
-        location: "Elgiganten A/S, DK",
-        description:
-            "Handled electronics sales, provided customer advice and technical support, and assisted with IT setup and troubleshooting.",
-        icon: React.createElement(CgWorkAlt),
-        date: "2017 - 2023",
-    },
+  {
+    title: "Graduated Health Technology Engineer",
+    location: "Aarhus University, DK",
+    description:
+      "Throuhout my studies, I took various software courses to learn about software development",
+    icon: React.createElement(LuGraduationCap),
+    date: "2020-2024",
+  },
+  {
+    title: "Engineering intern",
+    location: "Teknologi i Praksis, DK",
+    description:
+      "I was responsible for designing and 3D printing assistive devices. Gained experience in user involvement, technical presentations, and innovative problem-solving, with several solutions showcased in exhibitions.",
+    icon: React.createElement(CgWorkAlt),
+    date: "2023 - 2023",
+  },
+  {
+    title: "Sales assistant",
+    location: "Elgiganten A/S, DK",
+    description:
+      "Handled electronics sales, provided customer advice and technical support, and assisted with IT setup and troubleshooting.",
+    icon: React.createElement(CgWorkAlt),
+    date: "2017 - 2023",
+  },
 ] as const;
--- src/app/lib/types.ts
+++ src/app/lib/types.ts
@@ -1,26 +1,27 @@
-import { links } from './data';
+import { links } from "./data";
 import React from "react";
 
-export type SectionName = (typeof links)[number]['hash'];
+export type SectionName = (typeof links)[number]["hash"];
 
 export type ProjectInfo = {
-    title: string;
-    description: string;
-    techStack: string[];
-    imageUrl?: string;
-    github: string;
-    live: string;
-}
+  title: string;
+  description: string;
+  techStack: string[];
+  imageUrl?: string;
+  imageGifUrl?: string;
+  github: string;
+  live: string;
+};
 
 export type Link = {
-    nameEng: string;
-    hash: string;
-}
+  nameEng: string;
+  hash: string;
+};
 
 export type VerticalElementType = {
-    title: string;
-    description: string;
-    location: string;
-    date: string;
-    icon: React.ReactElement;
+  title: string;
+  description: string;
+  location: string;
+  date: string;
+  icon: React.ReactElement;
 };
